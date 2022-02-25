d517f57676fdd15bddde782047974dcb8524684f
Pratz03 <86201887+Pratz03@users.noreply.github.com>
2022-02-26 00:32:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import { selectUser } from './features/userSlice';
+import { useSelector } from 'react-redux';
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     //BEM naming convention - scalable and maintainable -> className="App" -> className="app"
     <div className="app">
 
-      {/* Sidebar */}
-      <Sidebar />
+      {user ? (
+        <>
+          {/* Sidebar */}
+          <Sidebar />
 
-      {/* Chat */}
-      <Chat />
+          {/* Chat */}
+          <Chat />  
+        </>
+      ) : (
+        <Login />
+      )}
+      
 
     </div>
   );
